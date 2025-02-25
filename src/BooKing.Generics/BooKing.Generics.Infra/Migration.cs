83c0a3181f68d78df28b7197d89f8671ce8ce3a7
gajalves <ga.jaimealves@gmail.com>
2025-02-25 15:18:12 -0300

    wip

--- src/BooKing.Generics/BooKing.Generics.Infra/Migration.cs
+++ src/BooKing.Generics/BooKing.Generics.Infra/Migration.cs
@@ -4,16 +4,39 @@ using Microsoft.Extensions.DependencyInjection;
 namespace BooKing.Generics.Infra;
 public static class Migration
 {
+    private static readonly int maxAttempts = 5;
+    static int attempt = 0;
+
     public static void RunMigration<T>(this IServiceProvider app) where T : DbContext
     {
         using (var serviceScope = app.CreateScope())
         {
-            var context = serviceScope.ServiceProvider.GetService<T>();            
+            var context = serviceScope.ServiceProvider.GetService<T>();
             context.Database.SetCommandTimeout(3 * 1000);
-            
-            if (context.Database.GetPendingMigrations().Any())
+
+            var delay = TimeSpan.FromSeconds(5);
+
+            while (attempt < maxAttempts)
             {
-                context.Database.Migrate();
+                try
+                {
+                    if (context.Database.CanConnect())
+                    {
+                        if (context.Database.GetPendingMigrations().Any())
+                        {
+                            context.Database.Migrate();
+                        }
+                        return;
+                    }
+                }
+                catch
+                {
+                    attempt++;
+                    if (attempt == maxAttempts)
+                        throw new Exception("Não foi possível conectar ao banco de dados após várias tentativas.");
+
+                    Thread.Sleep(delay);
+                }
             }
         }
     }
